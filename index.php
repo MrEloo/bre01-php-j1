fae860fcd92ec811e8c45930969ad234c4bcb085
Eloan <eloan.coindin@outlook.fr>
2024-01-10 15:13:35 +0100

    exercice 1 - 4 echo

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require('echo/head.phtml'); ?>
+
+
+
+
+
+<body>
+
+    <?php require('echo/article.phtml'); ?>
+    <?php require('echo/paragraphe.phtml'); ?>
+    <?php require('echo/exercice-4.phtml'); ?>
+
+</body>
+
+</html>
