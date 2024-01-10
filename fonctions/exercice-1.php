3f0fc122096b448fb035b8bc82bc177655d56a92
Eloan <eloan.coindin@outlook.fr>
2024-01-10 12:25:44 +0100

    "exercice 1 fonctions exercice 2 fonctions exercice 3 fonctions exercice 4 fonctions"

---
+++ fonctions/exercice-1.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+$concat = fn ($string1, $string2) => "$string1  $string2";
+
+
+echo $concat("Hello ", "World !<br>");
+echo $concat("À la ", "claire fontaine<br>");
+echo $concat("La vie, l'univers ", "et tout le reste<br>");
